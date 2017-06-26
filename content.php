e5503c23f29e975015f1c0101fd9594f5369075a
Marcus Kazmierczak <marcus@mkaz.com>
2017-06-25 20:17:00 -0700

    Add font awesome to meta (linefeeds)

--- content.php
+++ content.php
@@ -15,26 +15,29 @@
         </a>
     <?php endif; ?>
 
-    <div class="post-date">
-        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-            <?php the_time( get_option( 'date_format' ) ); ?>
-        </a>
+    <?php if ( get_post_type() == 'post' ) : ?>
+    <div class="meta">
+        <span class="post-date">
+            <span class="fa fa-calendar"></span>
+            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                <?php the_time( get_option( 'date_format' ) ); ?>
+            </a>
+        </span>
+        <span class="post-category">
+            <span class="fa fa-folder"></span>
+            <?php the_category( ', ' ); ?>
+        </span>
+        <span class="post-tags">
+            <?php the_tags( ' #', ' #', ' ' ); ?>
+        </span>
     </div>
+    <?php endif; ?>
 
     <div class="content">
         <?php the_content(); ?>
     </div>
 
     <?php if ( is_singular() ) { wp_link_pages(); }?>
-
-    <?php if ( get_post_type() == 'post' ) : ?>
-        <div class="meta">
-            <?php if ( is_singular( 'post' ) ) : ?>
-                <p><?php _e( 'In', 'postmodern' ); ?> <?php the_category( ', ' ); ?></p>
-                <p><?php the_tags( ' #', ' #', ' ' ); ?></p>
-            <?php endif; ?>
-        </div>
-    <?php endif; ?>
 
     <?php if ( is_singular() ) { comments_template(); } ?>
 
