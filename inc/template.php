c9c993b5b80e755e67a5b31f357c933666e64513
Marcus Kazmierczak <marcus@mkaz.com>
2017-11-16 14:18:56 -0800

    Fix for empty tags

--- inc/template.php
+++ inc/template.php
@@ -2,6 +2,9 @@
 
 function postmodern_get_tags() {
     global $post;
+    $tags = get_the_tags( $post->ID );
+    if ( ! is_array ( $tags ) ) { return; }
+
     foreach( get_the_tags( $post->ID ) as $tag ) {
         echo '<a href="' . get_tag_link( $tag->term_id ) . '">#' . $tag->name . '</a>';
     }
