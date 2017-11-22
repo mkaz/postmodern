8d15b16666a72f223c9f38ff9daa4f3ebce5063e
Marcus Kazmierczak <marcus@mkaz.com>
2017-11-21 19:41:38 -0800

    Update portfolio bits

--- portfolio-page.php
+++ portfolio-page.php
@@ -8,56 +8,54 @@
 get_template_part( 'template-parts/header' ); ?>
 
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<main class="portfolio">
 
-		<header class="entry-header">
-			<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-		</header>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+	</header>
 
-		<?php
+	<?php
 
-			// get tag from custom values
-			$custom_data = get_post_custom();
-			if ( isset( $custom_data['portfolio-category'] ) ) {
-				$categories = $custom_data['portfolio-category'];
-				if ( ! empty( $categories ) && is_array( $categories ) ) {
-					$category = trim( $categories[0] );
+	// get tag from custom values
+	$custom_data = get_post_custom();
+	if ( isset( $custom_data['portfolio-category'] ) ) {
+		$categories = $custom_data['portfolio-category'];
+		if ( ! empty( $categories ) && is_array( $categories ) ) {
+			$category = trim( $categories[0] );
 
-					$args = array(
-						'category_name' => $category,
-						'order' => 'DESC',
-					);
+			$args = array(
+				'category_name' => $category,
+				'order' => 'DESC',
+			);
 
-					$query = new WP_Query( $args );
+			$query = new WP_Query( $args );
 
-					// a loop
-					if ( $query->have_posts() ) {
+			// a loop
+			if ( $query->have_posts() ) {
 
-						echo '<div class="portfolio-wrapper">';
-						while ( $query->have_posts() ) {
-							$query->the_post();
-							get_template_part( 'template-parts/content', 'portfolio' );
-						}
-						echo '</div>';
-
-						wp_reset_postdata();
-					}
-					else {
-						echo '<p> Sorry, no posts matching <b>' . esc_html( $category ) . '</b> category </p>';
-					}
+				echo '<section class="portfolio-list">';
+				while ( $query->have_posts() ) {
+					$query->the_post();
+					get_template_part( 'template-parts/content', 'portfolio' );
 				}
-				else {
-					echo '<p>No category defined for this Portfolio page</p>';
-				}
+				echo '</section>';
+
+				wp_reset_postdata();
 			}
 			else {
-				echo '<p>Custom Field: <b>portfolio-category</b> not defined.</p>';
+				echo '<p> Sorry, no posts matching <b>' . esc_html( $category ) . '</b> category </p>';
 			}
+		}
+		else {
+			echo '<p>No category defined for this Portfolio page</p>';
+		}
+	}
+	else {
+		echo '<p>Custom Field: <b>portfolio-category</b> not defined.</p>';
+	}
 
-		?>
-		</main>
-	</div>
+	?>
+	</main>
 
 
 <?php get_template_part( 'template-parts/footer' ); ?>
--- template-parts/content-portfolio.php
+++ template-parts/content-portfolio.php
@@ -2,7 +2,7 @@
 /**
  * The template used for displaying projects on index view
  *
- * @package Blask
+ * @package Postmodern
  */
 
 ?>
@@ -11,7 +11,7 @@
 		<div class="portfolio-thumbnail">
 			<a href="<?php the_permalink(); ?>">
 				<?php if ( '' != get_the_post_thumbnail() ) : ?>
-					<?php the_post_thumbnail( 'blask-post-thumbnail' ); ?>
+					<?php the_post_thumbnail( 'large' ); ?>
 				<?php endif; ?>
 				<?php the_title( '<h2 class="project-title">', '</h2>' ); ?>
 			</a>
