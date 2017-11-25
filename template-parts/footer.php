62d5a762dae1a513ab1e4f9420a66b734af9c896
Marcus Kazmierczak <marcus@mkaz.com>
2017-11-25 08:29:41 -0800

    Add CC, split up stylesheet

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -17,7 +17,17 @@
 <?php endif; ?>
 
 <footer class="global">
-    <p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></p>
+    <section class="copyright">
+        <span> &copy; 1997-<?php echo date( 'Y' ); ?>
+            <a href="<?php echo esc_url( home_url() ); ?>" rel="copyright">mkaz.com</a>
+        </span>
+        <span class="creative-commons">
+            <a rel="license" href="https://creativecommons.org/licenses/by-nc/4.0/">
+            <img src="<?php echo get_template_directory_uri(); ?>/inc/by-nc.eu.svg"
+                width="88" alt="Creative Commons Attribution-NonCommercial 4.0 International License.">
+            </a>
+        </span>
+    </section>
     <p>Theme: <a href="https://github.com/mkaz/postmodern/">Postmodern</a> by <a href="https://mkaz.com/" rel="designer">mkaz</a></p>
 </footer>
 
