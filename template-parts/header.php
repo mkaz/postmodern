d7299e61fca5c8ad479bc317d6fbaa125a7f215a
Marcus Kazmierczak <marcus@mkaz.com>
2018-01-03 11:55:56 -0800

    Add microblog link

--- template-parts/header.php
+++ template-parts/header.php
@@ -4,6 +4,7 @@
         <meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>" charset="<?php bloginfo( 'charset' ); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
         <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link href="https://micro.blog/mkaz" rel="me" />
         <?php wp_head(); ?>
     </head>
 
